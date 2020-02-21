04e850d3da07321e753686395f46d96867ed109e
Axel Elmarsson <axelelm@kth.se>
2020-02-21 21:55:08 +0100

    Functioning login, no cookie generation though

--- server/auth/login.js
+++ server/auth/login.js
@@ -1,8 +1,46 @@
+'use strict'
+
 var express = require('express')
 var router = express.Router()
+const db = require('../database');
+const bcrypt = require('bcrypt');
 
-router.get('/', (req, res) => {
-    res.send({"login":"hi from login"})
+router.post('/', (req, res) => {
+    const {email, password} = req.body
+    loginUser(email, password, (err, msg, obj) => {
+        if (err) {
+            res.status(403)
+            if (err === 0) res.json({"error":"Invalid user data (empty?)", "payload":{}})
+            if (err === 1) res.json({"error":"Internal server error", "payload":{}})
+            if (err === 2) res.json({"error":"Wrong username or password", "payload":{}})
+            return;
+        }
+        res.status(200).json({"status":"OK", "error":null, "payload": {
+            //TODO: Send cookie (and user data?)
+        }})
+    })
 })
 
 module.exports = router
+
+
+const loginUser = (email, password, callback) => {
+    if (email.length === 0 || password.length === 0) {
+        return callback(0, 'One of the fields were empty, no user logged in.')
+    }
+
+    db.get('SELECT * FROM Users WHERE email = ?', [email], (err, row) => {
+        if (err) return callback(1, 'Internal server error.')
+        
+        if (row === undefined) {
+            return callback(2, 'User doesn\'t exist.')
+        }
+
+        bcrypt.compare(password, row.password, (err, equal) => {
+            if (equal) {
+                //TODO: Create cookie/token
+                return callback(null, `Logged in user ${row.username}.`, {"cookie":"In the future this is a cookie"})
+            } else return callback(2, 'Wrong password.')
+        })
+    })
+}
