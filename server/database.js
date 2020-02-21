33a5735a924952552cf6c4dbdd53f915953012ee
Axel Elmarsson <axelelm@kth.se>
2020-02-21 19:39:59 +0100

    User table

--- server/database.js
+++ server/database.js
@@ -6,9 +6,11 @@ const sqlite3 = require('sqlite3').verbose();
 const databasePath = path.join(__dirname, 'db.sqlite');
 const db = new sqlite3.Database(databasePath);
 
+const Users = 'Users(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, email TEXT, password TEXT, registration_date TEXT)'
+
 db.serialize(() => {
     // db.run('DROP TABLE IF EXISTS Users')
-    // db.run(`CREATE TABLE IF NOT EXISTS ${Users}`);
+    db.run(`CREATE TABLE IF NOT EXISTS ${Users}`);
 
 })
 
