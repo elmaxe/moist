d58af26d17ec6b9a8fe74987f6da0928cd48b340
Axel Elmarsson <axelelm@kth.se>
2020-02-25 20:42:21 +0100

    Move App, import reducers

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import combineReducers from './reducers/index'
 
 const store = createStore(
+    combineReducers,
     applyMiddleware(
         thunk,
         logger
