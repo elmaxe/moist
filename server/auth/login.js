06bb2e4d2179aacdc887b9e7f69e993d7af33389
Axel Elmarsson <axel.elmarsson@gmail.com>
2020-03-23 10:08:44 +0100

    User authentication
    *Create a cookie when logging in if there's no session.
    *Endpoint for checking if user cookie is valid.

--- server/auth/login.js
+++ server/auth/login.js
@@ -2,7 +2,6 @@
 
 var express = require('express')
 var router = express.Router()
-var errr = ""
 const db = require('../database');
 const bcrypt = require('bcrypt');
 
@@ -18,9 +17,9 @@ const validCredentials = (req, res, next) => {
 router.post('/', validCredentials, (req, res) => {
     console.log(req.body);
     const {email, password} = req.body;
-
+    
     const getUser = db.prepare('SELECT * FROM USERS WHERE email = ?')
-
+    
     getUser.get([email], (err, row) => {
         if (err) {
             res.status(500).json({"error":"Internal server error."})
@@ -36,6 +35,7 @@ router.post('/', validCredentials, (req, res) => {
                 return
             }
             if (equal) {
+                authenticate(req, row)
                 res.status(200).json({
                     id: row.id,
                     username: row.username,
@@ -49,6 +49,55 @@ router.post('/', validCredentials, (req, res) => {
             }
         })
     })
+    getUser.finalize()
 });
 
+router.get('/isAuth', (req, res) => {
+    const user = req.session.user
+
+    const getUser = db.prepare('SELECT * FROM Users WHERE id = ?')
+
+    if (user) {
+        getUser.get([user.id], (err, row) => {
+            if (err) {
+                res.status(500).json({"error":"Internal server error."})
+                return
+            }
+
+            if (row === undefined) {
+                res.status(404).json({"error":"Invalid session."})
+            }
+
+            res.status(200).json({
+                authenticated: true,
+                user: {
+                    id: row.id,
+                    email: row.email,
+                    username: row.username,
+                    regDate: row.regDate,
+                    profilePicture: row.profilePicture
+                }
+            })
+        })
+        getUser.finalize()
+    } else {
+        res.status(404).json({
+            authenticated: false,
+        })
+    }
+})
+
 module.exports = router
+
+const authenticate = (req, row) => {
+    if (!req.session.user) {
+        console.log("Create new session for user: " + row.username)
+        req.session.user = {
+            id: row.id,
+            email: row.email,
+            username: row.username
+        }
+    } else {
+        console.log(`User ${row.username} already has a session`)
+    }
+}
