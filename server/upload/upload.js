f790d17efb7411dd4de06a2b3284ecd89b57e9c8
Axel Elmarsson <axel.elmarsson@gmail.com>
2020-03-23 13:49:21 +0100

    File upload safety
    Check mime type of files to prevent uploading of malicious files.
    Only png and jpegs are allowed for now.

--- server/upload/upload.js
+++ server/upload/upload.js
@@ -4,6 +4,7 @@ var express = require('express')
 var router = express.Router()
 const uuid4 = require('uuid4');
 const fileUpload = require('express-fileupload')
+const fileType = require('file-type')
 
 router.use(fileUpload({
     createParentPath: true,
@@ -18,26 +19,33 @@ router.post('/', (req, res) => {
         res.status(400).json({"error":"No file provided, no file uploaded."})
     }
 
-    let sampleFile = req.files.sampleFile
-    let newName = uuid4() + ".png"
-    //Utgår tydligen från app:s directory och inte upload.js
-    sampleFile.mv('./images/' + newName, (err) => {
-        if (err) {
-            res.status(500).json({"error":"Internal server error."})
+    let imageFile = req.files.imageFile
+
+    fileType.fromBuffer(imageFile.data)
+    .then(result => {
+        if (result.mime !== 'image/jpeg' && result.mime !== 'image/png') {
+            res.status(403).json({"error":"File type not allowed."})
             return
         }
-
-        res.status(200).json({
-            "status":"File uploaded.",
-            image: {
-                name: newName,
-                mimetype: sampleFile.mimetype,
-                path: '/images/' + newName,
-                size: sampleFile.size
+        let newName = uuid4() + ".png"
+        //Utgår tydligen från app:s directory och inte upload.js
+        imageFile.mv('./images/' + newName, (err) => {
+            if (err) {
+                res.status(500).json({"error":"Internal server error."})
+                return
             }
+    
+            res.status(200).json({
+                "status":"File uploaded.",
+                image: {
+                    name: newName,
+                    mimetype: imageFile.mimetype,
+                    path: '/images/' + newName,
+                    size: imageFile.size
+                }
+            })
         })
     })
-
     // res.send("upload")
 });
 
