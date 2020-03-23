c6e7b5ce1399ff63eb1d70ae0256e06c6e3e6eb5
Axel Elmarsson <axel.elmarsson@gmail.com>
2020-03-23 09:23:01 +0100

    Garden SQL table

--- server/database.js
+++ server/database.js
@@ -8,11 +8,13 @@ db.exec("PRAGMA foreign_keys=ON")
 
 // Auto increment automatically increments the id entry, there is no need to supply it a value.
 const userTable = 'CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL, regDate TEXT, profilePicture TEXT)';
+const gardenTable = 'CREATE TABLE IF NOT EXISTS Gardens (gid INTEGER PRIMARY KEY AUTOINCREMENT, uid INTEGER, name TEXT NOT NULL, description TEXT, image TEXT, creationDate TEXT NOT NULL, location TEXT, FOREIGN KEY(uid) REFERENCES Users(id) ON DELETE CASCADE)'
 
 db.serialize(() => {
-    db.run('DROP TABLE IF EXISTS Users');
-    // db.run('DROP TABLE IF EXISTS Todos');
+    // db.run('DROP TABLE IF EXISTS Users');
+    // db.run('DROP TABLE IF EXISTS Gardens');
     db.run(userTable);
+    db.run(gardenTable)
 });
 
 module.exports = db;
