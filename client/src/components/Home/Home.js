ee9f606eca2f46d3c787391aa4ecc001ad50ea85
Axel Elmarsson <axel.elmarsson@gmail.com>
2020-03-30 17:43:35 +0200

    Basic bucket list

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,13 +1,47 @@
-import React from 'react'
-import Bar from '../Bar/Bar'
+import React, { useState } from 'react'
 
-const Home = (props) => {
-    console.log(props)
+import './Home.css'
+
+const Home = ({props}) => {
+    const [suggestion, setSuggestion] = useState(null)
+    const [fetching, setFetching] = useState(false)
+    const [bucketlist, setBucketlist] = useState([])
+
+    const save = () => {
+        setBucketlist(bucketlist.concat([suggestion]))
+    }
+
+    const getSuggestion = () => {
+        setFetching(true)
+        fetch('https://www.boredapi.com/api/activity', {
+            method: "GET"
+        })
+        .then(res => res.json())
+        .then(json => {
+            console.log(json)
+            setFetching(false)
+            setSuggestion(json)
+        })
+    }
+
     return (
         <div>
-            {/* <Bar state={props.state.userData}/> */}
-            <div style={{paddingTop: "50px"}}>
-                HOME
+            <div className="Home">
+                <div>
+                    <h1>My bucketlist</h1>
+                </div>
+                <div>
+                    <button onClick={getSuggestion} disabled={fetching} >Suggest an activity</button>
+                </div>
+                <div>
+                    {suggestion ? suggestion.activity : null}
+                </div>
+                <div>
+                    <button onClick={save}>Save to bucketlist</button>
+                </div>
+                <div>
+                    {bucketlist.map(x => <div>{x.activity}</div>)}
+                </div>
             </div>
         </div>
     )
