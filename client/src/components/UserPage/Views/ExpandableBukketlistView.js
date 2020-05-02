85f4e94d715ad405e225ecdcdf337d7ce8334db3
Axel Elmarsson <axel.elmarsson@gmail.com>
2020-05-02 18:34:41 +0200

    Fix with css

--- client/src/components/UserPage/Views/ExpandableBukketlistView.js
+++ client/src/components/UserPage/Views/ExpandableBukketlistView.js
@@ -18,7 +18,7 @@ const ExpandableBukketlistView = ({bukketlist, activities, hide, fetching, onCli
                         {fetching ? 
                                 <img src={spinner} style={{width: "80px"}} />
                             :
-                                activities.length === 0 ? "No activities" : activities.map(x => <div id="title" key={x.aid}>{x.activity}</div>)
+                                activities.length === 0 ? <div id="title">No activities</div> : activities.map(x => <div id="title" key={x.aid}>{x.activity}</div>)
                         }
                     </div>
                 :
