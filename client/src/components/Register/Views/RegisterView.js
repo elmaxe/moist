c9178a7b0614ce6a3d7993347352448ac782c84e
Axel Elmarsson <axel.elmarsson@gmail.com>
2020-05-08 23:22:45 +0200

    Small fixes

--- client/src/components/Register/Views/RegisterView.js
+++ client/src/components/Register/Views/RegisterView.js
@@ -12,7 +12,7 @@ const RegisterView = ({username, email, passwordOne, passwordTwo, error, success
             <div>
                 {error && <Alert variant="danger">{error}</Alert>}
                 {success && <Alert variant="primary">Registration successful! Sign in above.</Alert>}
-                <Alert variant="danger">Don't enter sensitive data in this form (or on this site) if there is no secure HTTPS connection. <a href="https://support.google.com/webmasters/answer/6073543" rel="noopener noreferrer" target="_blank">Learn more!</a></Alert>
+                <Alert id="httpwarning" variant="danger">Don't enter sensitive data in this form (or on this site) if there is no secure HTTPS connection. <a href="https://support.google.com/webmasters/answer/6073543" rel="noopener noreferrer" target="_blank">Learn more!</a></Alert>
                 <form>
                 <table>
                     <tbody>
