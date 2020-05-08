4cafca8807c7342b9c46d07ca00c7e94f024ba52
Axel Elmarsson <axel.elmarsson@gmail.com>
2020-05-08 22:19:43 +0200

    Improve no results page

--- client/src/components/SearchResultPage/Views/NoResults.js
+++ client/src/components/SearchResultPage/Views/NoResults.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import noresults from '../../../images/noresults2.svg'
 
 const NoResults = ({query}) => {
     return (
         <div className="NoResultsFound">
+            <img src={noresults} style={{width: "5vw", paddingBottom: "5vh"}}/>
             <div>We found no results for:</div>
             <span><b>"{query}"</b></span>
             <div>Try another search term.</div>
